50e0ca2763844121783c0d4e3607c8423f01952b
EKporon <ekporon@alustudent.com>
2023-09-29 11:51:37 -0700

    4-concat.js

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,13 +2,12 @@
 
 // Check if two arguments were provided
 if (process.argv.length !== 4) {
-    console.log("Usage: ./printArgs.js <arg1> <arg2>");
-    process.exit(1);
+    console.log("Usage: ./4-concat.js <arg1> <arg2>");
+} else {
+    // Get the two arguments
+    const arg1 = process.argv[2];
+    const arg2 = process.argv[3];
+
+    // Print the arguments in the specified format
+    console.log(`${arg1 ? arg1 : 'undefined'} is ${arg2 ? arg2 : 'undefined'}`);
 }
-
-// Get the two arguments
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-// Print the arguments in the specified format
-console.log(`${arg1} is ${arg2}`);
