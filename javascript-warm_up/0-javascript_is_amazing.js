2844733b0de2c88b97c046b3e267e82721187356
EKporon <ekporon@alustudent.com>
2023-09-29 08:45:51 -0700

    File modified for black spaces

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 // Define a constant variable called myVar with the value "JavaScript is amazing"
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 
 // Use console.log(...) to print the output
 console.log(myVar);
-
