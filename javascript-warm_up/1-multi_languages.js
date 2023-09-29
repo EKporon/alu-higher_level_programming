79e0830b3202c0102ab1e52ef9eee5bdc21959f1
EKporon <ekporon@alustudent.com>
2023-09-29 08:55:38 -0700

    Used single quote

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 // 1st line
-console.log("C is fun");
+console.log('C is fun');
 
 // 2nd line
-console.log("Python is cool");
+console.log('Python is cool');
 
 // 3rd line
-console.log("JavaScript is amazing");
+console.log('JavaScript is amazing');
