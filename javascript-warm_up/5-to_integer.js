792f1dd59ed497a23254926d2c3ea5a5ccc05770
EKporon <ekporon@alustudent.com>
2023-09-29 12:30:06 -0700

    Nice

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// Check if there is at least one command line argument
+if (process.argv.length < 3) {
+    console.log('Not a number');
+} else {
+    // Convert the first command line argument to an integer
+    const number = parseInt(process.argv[2], 10);
+
+    // Check if the conversion resulted in a valid integer
+    if (!isNaN(number)) {
+        console.log('My number: ' + number);
+    } else {
+        console.log('Not a number');
+    }
+}
