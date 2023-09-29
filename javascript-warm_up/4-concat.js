57883cafb78210ae4f0f44e5c273ce92a63fe452
EKporon <ekporon@alustudent.com>
2023-09-29 12:23:07 -0700

    Cant't indent with Tab key

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,15 +2,16 @@
 
 // Check how many arguments were provided
 if (process.argv.length === 4) {
-    // Two arguments provided
-    const arg1 = process.argv[2];
-    const arg2 = process.argv[3];
-    console.log(`${arg1} is ${arg2}`);
+  // Two arguments provided
+  const arg1 = process.argv[2];
+  const arg2 = process.argv[3];
+  console.log(`${arg1} is ${arg2}`);
 } else if (process.argv.length === 3) {
-    // One argument provided
-    const arg1 = process.argv[2];
-    console.log(`${arg1} is undefined`);
+  // One argument provided
+  const arg1 = process.argv[2];
+  console.log(`${arg1} is undefined`);
 } else {
-    // No arguments provided
-    console.log(`undefined is undefined`);
+  // No arguments provided
+  console.log('undefined is undefined');
 }
+
