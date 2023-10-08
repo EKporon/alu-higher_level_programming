8cae230858ae2d217132eecc71531f3eee8d34cf
EKporon <e.kporon@alustudent.com>
2023-10-08 19:39:00 +0000

    writings

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    process.exit(1); // Exit the script with a non-zero status code on error
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
-
