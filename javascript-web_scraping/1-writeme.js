8c8f0caeafb54dcbdb47abfbb9013a0cd0cb72e7
EKporon <e.kporon@alustudent.com>
2023-10-08 19:14:33 +0000

    Wm

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -8,5 +8,7 @@ const contentToWrite = process.argv[3];
 fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
   if (err) {
     console.error(err);
+    process.exit(1); // Exit the script with a non-zero status code on error
   }
 });
+
