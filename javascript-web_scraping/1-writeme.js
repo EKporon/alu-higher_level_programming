e7120178df4c7b97921ee6cd1c29f3fd6cde085f
EKporon <e.kporon@alustudent.com>
2023-10-08 18:56:19 +0000

    write me 2

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-
-fs.writeFileSync(filePath, stringToWrite, 'utf-8', function (error) {
-  if (error) {
-    console.error(error);
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
